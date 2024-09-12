261db8f0262d42d42fe620d8112bc5eec78ee26b
Hasna <hassna.ahmad98@gmail.com>
2024-09-12 13:27:48 +0300

    without condition

---
+++ main.js
@@ -0,0 +1,69 @@
+
+
+let image = document.getElementById("image")
+console.log(image)
+
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+let btn4 = document.getElementById("btn4")
+let btn5 = document.getElementById("btn5")
+let btn6 = document.getElementById("btn6")
+let btn7 = document.getElementById("btn7")
+let btn8 = document.getElementById("btn8")
+let btn9 = document.getElementById("btn9")
+
+
+
+
+btn1.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+})
+
+btn2.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
+})
+
+btn3.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
+})
+
+btn4.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg  "  ;
+})
+
+btn5.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+})
+
+btn6.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
+})
+
+btn7.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+})
+
+btn8.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg "   ;
+})
+
+btn9.addEventListener('click' , () => {
+    console.log("clicked")
+    image.src =" https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"    ;
+})
+
+/////////////////////////
+
+// let remote = document.getElementsByClassName("channel")
+// console.log(remote)
+
+// for ()
